d22122593b22fdd27764e3989b32e43a220ec9c0
Cool-ShowTTV <22648256+Cool-showTTV@users.noreply.github.com>
2022-02-12 14:41:39 -0500

    mess of code update lol

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 include "requests.php";
 
 // Checks if the user gives a team number
-if (isset($_GET['num']) || isset($_POST['num'])){
+if (isset($_GET['num']) || isset($_POST['num']) || isset($_GET['team']) || isset($_POST['team'])){
     // The messiest way to get input I have ever done
     if (isset($_POST['num'])){
         $number = $_POST['num'];
     }elseif (isset($_GET['num'])){
         $number = $_GET['num'];
     }elseif (isset($_POST['team'])){
-        $number = $_POST['num'];
+        $number = $_POST['team'];
     }elseif (isset($_GET['team'])){
-        $number = $_GET['num'];
+        $number = $_GET['team'];
     }
 
     $teamInfo = sendRequest("https://www.thebluealliance.com/api/v3/team/frc{$number}");
