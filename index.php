eea99c27f43dc718029574419e21e9b206e46e25
Cool-ShowTTV <22648256+Cool-showTTV@users.noreply.github.com>
2022-02-14 16:02:15 -0500

    move all SVG (more info below in comments)

--- index.php
+++ index.php
@@ -3,26 +3,28 @@ include "requests.php";
 $year = date("Y");//Get current year
 $svgTextSpace = 15;
 
-// Check if user put a custom color
-if (isset($_GET['color'])){
-    $svgColor = $_GET['color'];
-}else{
-    $svgColor = "d7d3cb";
-}
 
-header('Content-Type: image/svg+xml');
-echo '<svg
-    height="45"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
->';
 
 if (isset($_GET['num']) || isset($_GET['team'])){
+    header('Content-Type: image/svg+xml');
+    echo '<svg
+        height="45"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >';
+
     // The messiest way to get input I have ever done don't look
     if (isset($_GET['num'])){
         $number = $_GET['num'];
     }elseif (isset($_GET['team'])){
         $number = $_GET['team'];
+    }
+    
+    // Check if user put a custom color
+    if (isset($_GET['color'])){
+        $svgColor = $_GET['color'];
+    }else{
+        $svgColor = "d7d3cb";
     }
 
     // Get a list of all upcoming events for a team number
@@ -50,11 +52,10 @@ if (isset($_GET['num']) || isset($_GET['team'])){
         echo "<text y=\"$svgTextSpace\"  font-size=\"15\" font-family=\"Courier, Monospace\" fill=\"#$svgColor\">$formatedName $formatedStartDate-$formatedEndDate</text>\n";
         $svgTextSpace += 15;
     }
+    // End SVG file
+    echo '</svg>';
 }else{
     // I am going to add a home page one I get a chance but for now it's going to out and error
     http_response_code(400);
     echo json_encode(array("Error"=>"Failed: no 'num' status", "Note"=>"A home page is coming soon I just have not done it yet"),JSON_PRETTY_PRINT);
 }
-
-// End SVG file
-echo '</svg>';
