64326445bc87cd00ffd9927454a7bdde25d8dbf5
Cool-ShowTTV <22648256+Cool-showTTV@users.noreply.github.com>
2022-02-16 08:06:28 -0500

    makeURL now makes the url

--- pages/scripts/makeURL.js
+++ pages/scripts/makeURL.js
@@ -1,16 +1,24 @@
 // make button run a function
-function createUrl(){
+async function createUrl(){
     frcNumber = document.getElementById("frcNumber").value;
-    github = document.getElementById("github");
+    fontName = document.getElementById("fontName").value;
     color = document.getElementById("txtColor").value;
-
-    if(github.checked){}
+    github = document.getElementById("github").checked;
+    
+    fontName = encodeURIComponent(fontName);
+    color = color.replace("#", "");
     console.log("createUrl");
-    console.log({
-            'frcNumber': frcNumber,
-            'github': github.checked,
-            'color': color
-        });
+    console.log({'frcNumber': frcNumber,'fontName': fontName,'color': color,'github': github});
+        
+    // copy url to clipboard
+    var url = document.location.origin + "/?font=" + fontName + "&num=" + frcNumber + "&color=" + color;
+    if(github){url = `![events](${url}&github=true)`}
+    console.log(url);
+    copyTextToClipboard(url);
+    
+    setTimeout(function(){
+        removeMessage();
+    }, 1000);
 }
 
 
